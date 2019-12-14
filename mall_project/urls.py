a271b637bf9da2238c55b3bcb58b03c78b14480b
khs0806 <55686024+khs0806@users.noreply.github.com>
2019-12-14 18:13:39 +0900

    order 폼,뷰,정보조회 추가

--- mall_project/urls.py
+++ mall_project/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from fcuser.views import index, RegisterView, LoginView
 from product.views import ProductList, ProductCreate, ProductDetail
+from order.views import OrderCreate, OrderList
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
@@ -25,5 +26,7 @@ urlpatterns = [
     path('product/', ProductList.as_view()),
     path('product/<int:pk>/', ProductDetail.as_view()),
     path('product/create/', ProductCreate.as_view()),
-
+    path('order/', OrderList.as_view()),
+    path('order/create/', OrderCreate.as_view()),
+    
 ]
--- order/forms.py
+++ order/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 from .models import Order
+from product.models import Product
+from fcuser.models import Fcuser
+from django.db import transaction
 
 class RegisterForm(forms.Form):
+    def __init__(self, request, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.requset = request
     
     quantity = forms.IntegerField(
         error_messages={
@@ -11,10 +17,31 @@ class RegisterForm(forms.Form):
     product = forms.IntegerField(
         error_messages={
             'required':'상품설명을 입력해주세요.'
-        }, label='상품설명', widjet=forms.HiddenInput
+        }, label='상품설명', widget=forms.HiddenInput
     )
     
 
     def clean(self):
         cleaned_data = super().clean()
+        quantity =cleaned_data.get('quantity')
+        product =cleaned_data.get('product')
+        fcuser = self.requset.session.get('user')
+
+        if quantity and product and fcuser:
+            with transaction.atomic():
+                prod = Product.objects.get(pk=product)
+                order = Order(
+                    quantity=quantity,
+                    product=prod,
+                    fcuser=Fcuser.objects.get(email=fcuser)
+                )
+                order.save()
+                prod.stock -= quantity
+                prod.save()
+        else:
+            self.product = product
+            self.add_error('quantity', '값이 없습니다')
+            self.add_error('product', '값이 없습니다')
+
+
         
